dd7bc7935a5fb8da0ca0d5311ad674c67538e4fc
URAJUV <urajuv1984@gmail.com>
2026-06-04 02:04:10 +0530

    Update monitor.js

--- monitor.js
+++ monitor.js
@@ -1,3 +1,4 @@
+/*
 const sendTelegram = require("./telegram");
 
 (async () => {
@@ -6,4 +7,42 @@ const sendTelegram = require("./telegram");
   );
 
   console.log("Success");
+})();
+*/
+const portfolio = require("./portfolio.json");
+
+const getNews = require("./news");
+const analyze = require("./sentiment");
+const sendTelegram = require("./telegram");
+
+(async () => {
+
+
+
+
+  for (const stock of portfolio.stocks) {
+
+    const news = await getNews(stock);
+
+    const result = await analyze(stock, news);
+
+    const parsed = JSON.parse(result);
+
+    if (
+      parsed.impact === "high" &&
+      parsed.sentiment !== "neutral"
+    ) {
+
+      await sendTelegram(
+`
+${parsed.sentiment.toUpperCase()} ALERT
+
+${stock}
+
+${parsed.summary}
+`
+      );
+    }
+  }
+
 })();
