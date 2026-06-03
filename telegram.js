29504646e00f71a6cbad710f8bb2a489ab73189f
URAJUV <urajuv1984@gmail.com>
2026-06-04 01:43:22 +0530

    Create telegram.js

---
+++ telegram.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+async function sendTelegram(message) {
+  const token = process.env.TELEGRAM_TOKEN;
+  const chatId = process.env.TELEGRAM_CHAT_ID;
+
+  await axios.post(
+    `https://api.telegram.org/bot${token}/sendMessage`,
+    {
+      chat_id: chatId,
+      text: message
+    }
+  );
+}
+
+module.exports = sendTelegram;
